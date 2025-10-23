b40d6027b85b22652528ab870f70fbd52639f43f
enesdemir72 <250541047@firat.edu.tr>
2025-10-23 18:28:07 +0300

    Update UcMesaj.java

---
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Java'ya Hosgeldiniz");
+        System.out.println("Java'ya Hosgeldiniz");
+        System.out.println("Java'ya Hosgeldiniz");
+       
+    }
+}
