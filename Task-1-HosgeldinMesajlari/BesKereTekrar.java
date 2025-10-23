5cf22025d1b751c7cbf9eb041b773ace34253c4a
enesdemir72 <250541047@firat.edu.tr>
2025-10-23 18:26:36 +0300

    Update BesKereTekrar.java

--- Task-1-HosgeldinMesajlari/BesKereTekrar.java
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -1,8 +1,10 @@
-public class DesenCiz {
+
+class Main {
     public static void main(String[] args) {
-        System.out.println("J   A   V   V   A");
-        System.out.println("J  A A   V V   A A");
-        System.out.println("J J AAAAA  V V  AAAAA");
-        System.out.println("J J A   A   V   A   A");
+        System.out.println("Java'ya Hosgeldiniz");
+        System.out.println("Java'ya Hosgeldiniz");
+        System.out.println("Java'ya Hosgeldiniz");
+        System.out.println("Java'ya Hosgeldiniz");
+        System.out.println("Java'ya Hosgeldiniz");
     }
 }
